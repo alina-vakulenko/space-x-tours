ed8bdbdbc2762ad73d5a0ae3e3b8c983778d1d0b
alina-vakulenko <alina.vakulenko12@gmail.com>
2023-12-14 20:04:26 +0200

    changed vite configurtion

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          sprite: ["/src/assets/sprite.svg"],
-        },
-      },
-    },
+    assetsDir: "assets",
   },
 });
